2ff8a44aeb3ee650b09d52fe87e573e09fb9afa7
Mateo Peñaranda <56937766+mattpding11@users.noreply.github.com>
2022-04-14 00:47:11 -0500

    Update conn.php

--- Gestionador #2/Config/conn.php
+++ Gestionador #2/Config/conn.php
@@ -3,8 +3,8 @@
 
 $host = "localhost"; # or 127.0.0.1 -> corresponde al host de la conexion
 $bd = "bd_universidad"; # correpondiente al nombre de la base de datos
-$user = "root"; # Nombre del usuario configurado en la base de datos
-$password = "root"; # Clave del usuario configurado para acceder a la base de datos
+$user = ""; # Nombre del usuario configurado en la base de datos
+$password = ""; # Clave del usuario configurado para acceder a la base de datos
 
 # Establecer conexion con la base de datos
 
